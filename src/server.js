279a85b9ba547b828f80feacdc79054facb61e86
Leonardo Pedro <leonardodeoliveira.tds1.senai@gmail.com>
2024-08-22 11:40:23 -0300

    Finish aula05

--- src/server.js
+++ src/server.js
@@ -13,6 +13,14 @@ app.get("/", (req, res) => {
     return res.status(200).send({ message: "Hello World!"})
 })
 
+app.post("/ ", (req, res) => {
+    return res.status(709).send({ message: "Hello World"})
+})
+
+app.post("/2tds1", (req, res) => {
+    return res.status(709).send({ message: "Hello, 2TDS1 - Só os Devs!"})
+})
+
 app.listen(port, () => {
     console.log(`🧨 server started on http://localhost:${port}`)
 })
