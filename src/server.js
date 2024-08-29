2a0873a258515ec8c759ce10c7053db87aa93012
Leonardo Pedro <leonardodeoliveira.tds1.senai@gmail.com>
2024-08-29 15:51:20 -0300

    Feat: add some requests

--- src/server.js
+++ src/server.js
@@ -9,11 +9,59 @@ const app = express()
 
 app.use(express.json())
 
+const guloseimas = [
+    {
+        id: 1,
+        nome: "Trufa",
+        preco: 8.5,
+},{
+    id: 2,
+    nome: "Palha italiana",
+    preco: 10.00,
+},
+{
+    id: 3,
+    nome: "Banoffee",
+    preco: 12.5,
+}
+]
+
+const filmesMarcantes = [
+    {
+        id: 1001,
+        titulo: "Meu Malvado Favorito 2",
+        genero: "Animação, Ação, Aventura",
+        emCartaz: false
+    },
+    {
+        id: 1002,
+        titulo: "Deus não está morto",
+        genero: "Romance, Drama e Aventura",
+        emCartaz: false,
+    },
+
+    {
+        id: 1003,
+        titulo: "Carros 5",
+        genero: "Animação",
+        emCartaz: true,
+    },
+
+]
+
 app.get("/", (req, res) => {
-    return res.status(200).send({ message: "Hello World!"})
+    return res.status(200).send({ message: "Hello World!"});
 })
 
-app.post("/ ", (req, res) => {
+app.get("/doces", (req, res) => {
+    return res.status(200).send(guloseimas);
+})
+
+app.get("/filmes", (req, res) => {
+    return res.status(200).send(filmesMarcantes);
+})
+
+app.post("/ ", (req, res) => {''
     return res.status(709).send({ message: "Hello World"})
 })
 
